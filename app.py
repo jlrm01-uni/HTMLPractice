d348161cb62d80bf4554bd733549adf129aee830
jlrm01-uni <jorge.rodriguez@guayama.inter.edu>
2024-11-22 11:01:01 -0400

    Added news page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@app.route("/news")
+def news():
+    return render_template("news.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
